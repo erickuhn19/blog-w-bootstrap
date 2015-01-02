b4f3df5dbe9b450703f0ed87a921e7d2d9849425
Eric Kuhn <erickuhn19@gmail.com>
2015-01-02 10:54:11 -0500

    Added comments and devise for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  devise_for :users
   resources :posts do
   resources :comments
   end
@@ -7,3 +7,4 @@ Rails.application.routes.draw do
   root "posts#index"
   get '/about', to: 'pages#about'
 end
+
