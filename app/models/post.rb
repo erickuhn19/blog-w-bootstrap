f2f15a7df808d53e2bd25408faf174f46b46faa1
Eric Kuhn <erickuhn19@gmail.com>
2015-01-03 15:45:24 -0500

    Modified post.rb to make more friendly url's for posts that include title

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
+
+	def to_param
+    "#{id}-#{title.parameterize}"
+  end
+
 end
